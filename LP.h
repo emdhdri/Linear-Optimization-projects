3a3637237ed860a2722e949cbbad281af1c68e47
Emad <emdhdri@gmail.com>
2023-11-03 19:55:38 +0330

    add makeCanonical function for LP class

--- LP.h
+++ LP.h
@@ -1,31 +1,149 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
 class lpProblem{
-    //If the problem is a maximizing problem then problem_type is 1 an otherwise its value is 0.
+    //If the problem is a maximizing problem then problem_type is 1 an otherwise its value is 2.
     int variables_count, constraints_count, problem_type;
     int *objective_function;
-    int **constraints;
+    int **constraint_matrix;
+    string *inequalities_type;
+    int *constraint_vector;
+    vector<int> nonzero_indexes;
 public:
     lpProblem(){
         variables_count = 0;
         constraints_count = 0;
         problem_type = -1;
         objective_function = nullptr;
-        constraints = nullptr;
+        constraint_matrix = nullptr;
+        inequalities_type = nullptr;
+        constraint_vector = nullptr;
+
     }
+
     void setProblemInfo(int _variables_count,int _constraints_count,int _problem_type){
         variables_count = _variables_count;
         constraints_count = _constraints_count; 
         problem_type = _problem_type;
     }
+
     void setObjectiveFunction(int *_objective_function){
         objective_function = _objective_function;
         _objective_function = nullptr;
-    }
-    void setProblemConstraints(int **_constraints){
-        constraints = _constraints;
-        _constraints = nullptr;
-    }
-    void displayProblem(){
-        
+        for(int i = 0; i < variables_count; i++){
+            if(objective_function[i] != 0){
+                nonzero_indexes.push_back(i);
+            }
+        }
     }
 
+    void setProblemConstraints(int **_constraint_matrix, string *_inequalities_type, int *_constraint_vector){
+        constraint_matrix = _constraint_matrix;
+        inequalities_type = _inequalities_type;
+        constraint_vector = _constraint_vector;
+        _constraint_matrix = nullptr;
+        _inequalities_type = nullptr;
+        _constraint_vector = nullptr;
+
+    }
+
+    void displayProblem(string variable);
+    void makeCanonical();
+
+};
+
+void lpProblem::displayProblem(string variable = "X"){
+    cout << "--------------------------------------\n";
+    if(problem_type == 1){
+        cout << "Maximize: \n";
+    }
+    else{
+        cout << "Minimize: \n";
+    }
+    for(int i = 0; i < nonzero_indexes.size(); i++){
+        cout << "(" <<objective_function[i] << "*" << variable << i + 1 << ")";
+        if(i < nonzero_indexes.size() - 1){
+            cout << " + ";
+        }
+    }
+    cout << "\nSubject to: \n";
+    for(int i = 0; i < constraints_count; i++){
+        for(int j = 0; j < variables_count; j++){
+            cout << "(" << constraint_matrix[i][j] << "*" << variable << j + 1 << ") ";
+            if(j < variables_count - 1){
+                cout << "+ ";
+            }
+        }
+        cout << inequalities_type[i] << " ";
+        cout << constraint_vector[i] << "\n";
+    }
+    for(int i = 0; i < variables_count; i++){
+        cout << "X" << i + 1;
+        if(i < variables_count - 1){
+            cout << ", ";
+        }
+    }
+    cout << " >= " << 0 << "\n";
+
+    cout << "--------------------------------------\n";
+}
+
+void lpProblem::makeCanonical(){
+    for(int i = 0; i < constraints_count; i++){
+        if(inequalities_type[i] == "="){
+            inequalities_type[i] = "<=";
+            constraints_count++;
+            int **tmp_constraint_matrix = new int*[constraints_count], *tmp_constraint_vector = new int[constraints_count];
+            for(int j = 0; j < constraints_count; j++){
+                tmp_constraint_matrix[j] = new int[variables_count];
+            }
+            string *tmp_inequalities_type = new string[constraints_count];
+            for(int j = 0; j < constraints_count - 1; j++){
+                tmp_constraint_matrix[j] = constraint_matrix[j];
+                tmp_inequalities_type[j] = inequalities_type[j];
+                tmp_constraint_vector[j] = constraint_vector[j];
+            }
+            for(int j = 0; j < variables_count; j++){
+                tmp_constraint_matrix[constraints_count - 1][j] = -constraint_matrix[i][j];
+            }
+            tmp_inequalities_type[constraints_count - 1] = "<=";
+            tmp_constraint_vector[constraints_count - 1] = -constraint_vector[i];
+            constraint_matrix = tmp_constraint_matrix;
+            inequalities_type = tmp_inequalities_type;
+            constraint_vector = tmp_constraint_vector;
+            tmp_constraint_matrix = nullptr;
+            tmp_inequalities_type = nullptr;
+            tmp_constraint_vector = nullptr;
+        }
+        else if(inequalities_type[i] == ">" || inequalities_type[i] == ">="){
+            for(int j = 0; j < variables_count; j++){
+                constraint_matrix[i][j] *= -1;
+            }
+            if(inequalities_type[i] == ">"){
+                inequalities_type[i] = "<";
+            }
+            else{
+                inequalities_type[i] = "<=";
+            }
+            constraint_vector[i] *= -1;
+        }
+    }
+    if(problem_type == 2){
+        for(int i = 0; i < constraints_count; i++){
+            if(inequalities_type[i] == "<"){
+                inequalities_type[i] = ">";
+            }
+            else{
+                inequalities_type[i] = ">=";
+            }
+            for(int j = 0; j < variables_count; j++){
+                constraint_matrix[i][j] *= -1;
+            }
+            constraint_vector[i] *= -1;
+        }
+    }
 }
--- dual.cpp
+++ dual.cpp
@@ -1,25 +1,56 @@
 #include <iostream>
 #include <string>
+#include "LP.h"
 
 using namespace std;
-
-class lpProblem{
-    int variables_count, constraints_count, problem_type;
-    
-}
 
 string MENU = "commands: \n1. Compute Dual \n2. Exit \nEnter the command number: ";
 
 void LP_dual(){
+    lpProblem p;
+
+    cout << "problem information.\n";
     int variables_count, constraints_count, problem_type;
-    cout << "Enter decision variables count: ";
-    cin >> variables_count;
-    cout << "Enter constraints count: ";
-    cin >> constraints_count;
-    cout << "If the problem is maximize problem enter 1 otherwise enter 0: ";
-    cin >> problem_type;
+    while(true){
+        cout << "Enter decision variables count: ";
+        cin >> variables_count;
+        cout << "Enter constraints count: ";
+        cin >> constraints_count;
+        cout << "Problem type:\n1. maximize\n2. minimize\nchoose problem type: ";
+        cin >> problem_type;
+        if(variables_count < 0 || constraints_count < 0 || (problem_type != 1 && problem_type != 2)){
+            cout << "Invalid information!\n";
+        }
+        else{
+            p.setProblemInfo(variables_count, constraints_count, problem_type);
+            break;
+        }
+    }
 
+    int *objective_function = new int[variables_count], *constraint_vector = new int[constraints_count];
+    int **constraint_matrix = new int*[constraints_count];
+    string *inequalities_type = new string[constraints_count];
+    for(int i = 0; i < constraints_count; i++){
+        constraint_matrix[i] = new int[variables_count];
+    }
+    cout << "Enter Objective Function: \n";
+    for(int i = 0; i < variables_count; i++){
+        cin >> objective_function[i];
+    }
+    p.setObjectiveFunction(objective_function);
 
+    cout << "Constraints \n";
+    for(int i = 0; i < constraints_count; i++){
+        cout << "Enter constraint number " << i + 1 << "\n";
+        for(int j = 0; j < variables_count; j++){
+            cin >> constraint_matrix[i][j];
+        }
+        cin >> inequalities_type[i];
+        cin >> constraint_vector[i];
+    }
+    p.setProblemConstraints(constraint_matrix, inequalities_type, constraint_vector);
+    p.makeCanonical();
+    p.displayProblem();
 }
 
 int main(){
